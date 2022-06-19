bb66b2565072d6609ab03d1026670bc2a1cf598a
Adamonis Arseni <arsenaloo97@gmail.com>
2022-06-20 00:38:58 +0300

    add logo and footer

--- project-react-app/src/components/Footer/Footer.jsx
+++ project-react-app/src/components/Footer/Footer.jsx
@@ -1,7 +1,23 @@
 import style from './Footer.module.css';
+import img1 from '../../IMG/log1.png';
+import img2 from '../../IMG/log2.png';
+import img3 from '../../IMG/log3.png';
+
 
 export default function Footer(props) {
 
-    return <footer className={style.header}>
+    return <footer className={style.footer}>
+        <div className={style.div}>
+            <img src={img1} alt=""/>
+            <span className={style.span}>+375 (29) 123-45-67</span>
+        </div>
+        <div className={style.div}>
+            <img src={img2} alt=""/>
+            <span className={style.span}>Минск, пр. Независимости 4</span>
+        </div>
+        <div className={style.div}>
+            <img src={img3} alt=""/>
+            <span className={style.span}>adamandeve@gmail.com</span>
+        </div>
     </footer>;
 };
--- project-react-app/src/components/Header/Header.jsx
+++ project-react-app/src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import style from './Header.module.css';
+import img from '../../IMG/logo.png'
 
 export default function Header(props) {
 
     return <header className={style.header}>
+        <img src={img} alt=""/>
         <h1 className={style.h1}>Adam & Eve</h1>
     </header>;
 };
