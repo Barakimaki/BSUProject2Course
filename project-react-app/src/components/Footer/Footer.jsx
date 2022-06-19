d5d829ded3b1a2ca0f3507b60363395c920961f5
Adamonis Arseni <arsenaloo97@gmail.com>
2022-06-20 00:42:09 +0300

    footer update

--- project-react-app/src/components/Footer/Footer.jsx
+++ project-react-app/src/components/Footer/Footer.jsx
@@ -9,15 +9,15 @@ export default function Footer(props) {
     return <footer className={style.footer}>
         <div className={style.div}>
             <img src={img1} alt=""/>
-            <span className={style.span}>+375 (29) 123-45-67</span>
+            <a href="#" className={style.link}>+375 (29) 123-45-67</a>
         </div>
         <div className={style.div}>
             <img src={img2} alt=""/>
-            <span className={style.span}>Минск, пр. Независимости 4</span>
+            <a href="#" className={style.link}>Минск, пр. Независимости 4</a>
         </div>
         <div className={style.div}>
             <img src={img3} alt=""/>
-            <span className={style.span}>adamandeve@gmail.com</span>
+            <a href="#" className={style.link}>adamandeve@gmail.com</a>
         </div>
     </footer>;
 };
